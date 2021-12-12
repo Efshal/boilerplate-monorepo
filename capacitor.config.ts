66de65a196e5fe9e0c9e35ad05e9c0ab1f6ac72c
efshal <efshalahmad@gmail.com>
2021-12-12 13:13:25 +0500

    add: android built

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'boilerplate-monorepo',
+  webDir: 'dist/boilerplate-monorepo',
+  bundledWebRuntime: false,
+};
+
+export default config;
